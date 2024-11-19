736d77d28841e6a0bddd4fefbc7ac4033adeecd4
kimkyeonghwan-1 <k2sc2000@naver.com>
2024-11-20 01:09:03 +0900

    feat: maps, exchanges 조회 함수만 구현

--- Back/exchanges/urls.py
+++ Back/exchanges/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-
+    path('', views.exchange_list),
 ]
--- Back/exchanges/views.py
+++ Back/exchanges/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+# permission Decorators
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+from django.shortcuts import get_object_or_404, get_list_or_404
+
+from .serializers import ExchangeSerializers
+from .models import Exchange
+
+
+# GET - 환율 조회
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def exchange_list(request):
+    if request.method == 'GET':
+        exchanges = get_list_or_404(Exchange)
+        serializer = ExchangeSerializers(exchanges, many=True)
+        return Response(serializer.data)
+
