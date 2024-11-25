d02eb1ae5cc6a0d4d4144823b74c4bb6c8f1202b
kimkyeonghwan <k2sc2000@naver.com>
2024-11-25 17:54:46 +0900

    feat : 지원금 추천 로직 구현

--- Back/subsidies/views.py
+++ Back/subsidies/views.py
@@ -106,6 +106,43 @@ def subsidy_list(request):
     return Response(response_data, status=status.HTTP_200_OK)
 
 
+from django.forms.models import model_to_dict
+# 보조금 추천 상품 목록 조회
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def subsidy_recommend_list(request):
+    subsidies = get_list_or_404(Subsidy)
+    serializers = SubsidyListSerializers(subsidies, many=True)
+    reco_subsidies = []
+
+    current_user = request.user
+    # 현재 유저의 모든 필드를 딕셔너리로 변환
+    current_user_data = model_to_dict(current_user)
+
+    for serializer in serializers.data:
+        print(serializer)
+        cnt = 0
+        if len(serializer['liked_users_info']) > 0:
+            for like_user in serializer['liked_users_info']:
+                print('아아아', like_user['birthyear'])
+                print('아아아2', current_user_data['birthyear'])
+                if current_user_data['birthyear']-5 <= like_user['birthyear'] <= current_user_data['birthyear']+5:
+                    cnt += 2
+                if like_user['income'] == current_user_data['income']:
+                    cnt += 2
+                if like_user['career'] == current_user_data['career']:
+                    cnt += 1
+                if like_user['region'] == current_user_data['region']:
+                    cnt += 1
+            reco_subsidies.append((cnt, serializer))
+        else:
+            reco_subsidies.append((0, serializer))
+    # 정렬 시, 점수를 기준으로 정렬
+    reco_subsidies.sort(key=lambda x: x[0], reverse=True)
+
+    return Response([subsidy for _, subsidy in reco_subsidies], status=status.HTTP_200_OK)
+
+
 # 보조금 상세 페이지
 @api_view(['GET','PUT'])
 @permission_classes([IsAuthenticated])
